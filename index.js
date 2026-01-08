360e3b53a88089aa61138058f5edbdba3dfc0ee9
23120074 <nguyenvanphuoc1172@gmail.com>
2026-01-09 01:36:50 +0700

    Test trigger

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 app.get('/', (req, res) => {
-  res.send('Hello DevOps CI/CD with Jenkins & Docker 🚀');
+  res.send('Hello! This is DevOps CI/CD with Jenkins & Docker 🚀');
 });
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
